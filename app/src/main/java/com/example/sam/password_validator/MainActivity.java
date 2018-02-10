c5ac57d3332c9380dc81b7c076c355b9b3600259
Samblam <sam.barefoot@dal.ca>
2018-02-10 04:02:56 -0400

    Finishing project

--- app/src/main/java/com/example/sam/password_validator/MainActivity.java
+++ app/src/main/java/com/example/sam/password_validator/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.sam.password_validator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText Password = findViewById(R.id.Password);
+        final Button Validate = findViewById(R.id.validation);
+        final TextView evaluation = findViewById(R.id.evaluation);
+        Validate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Validator.validate(String.valueOf(Password.getText())) == 5){
+
+
+                    evaluation.setText("Password is Strong");}
+                else{
+                    evaluation.setText("Password is Not Strong");
+                }
+            }
+        });
+
     }
 }
